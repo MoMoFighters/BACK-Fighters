28d228b1e5a5ad7fda92da95e527ff5c66bf77cd
mnppi223 <mnppi223@gmail.com>
2026-05-27 13:24:06 +0900

    [FEAT] 관리자 통계량 조회

--- legend-momo-city/src/main/java/com/wanted/momocity/admin/application/package-info.java
+++
@@ -1,2 +0,0 @@
-/** 관리자 페이지 - 응용 계층 (대시보드/로그 조회 유스케이스). */
-package com.wanted.momocity.admin.application;
--- legend-momo-city/src/main/java/com/wanted/momocity/admin/presentation/package-info.java
+++
@@ -1,2 +0,0 @@
-/** 관리자 페이지 - 표현 계층 (HTTP 컨트롤러, 요청/응답 DTO). */
-package com.wanted.momocity.admin.presentation;
