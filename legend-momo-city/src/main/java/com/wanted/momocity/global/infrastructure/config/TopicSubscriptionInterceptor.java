4231be87a3701755fcbf01f147ae6ef1d652a3b5
mosungjin <msj69076907@gmail.com>
2026-05-28 16:15:54 +0900

    [PR] 강의 등록 이벤트 구현 (모성진)

--- legend-momo-city/src/main/java/com/wanted/momocity/global/infrastructure/config/TopicSubscriptionInterceptor.java
+++ legend-momo-city/src/main/java/com/wanted/momocity/global/infrastructure/config/TopicSubscriptionInterceptor.java
@@ -1,5 +1,6 @@
 package com.wanted.momocity.global.infrastructure.config;
 
+import com.wanted.momocity.auth.application.port.LoadUserPort;
 import com.wanted.momocity.message.application.manager.ChatRoomSessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
