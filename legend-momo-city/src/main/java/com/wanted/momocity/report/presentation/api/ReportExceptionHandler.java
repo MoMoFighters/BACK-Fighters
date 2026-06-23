4f0c44bf99a2fa92557f403a22775bbe631a4b36
mnppi223 <mnppi223@gmail.com>
2026-06-23 17:09:19 +0900

    [REFACT] global 전역 예외 처리 수정

--- legend-momo-city/src/main/java/com/wanted/momocity/report/presentation/api/ReportExceptionHandler.java
+++ legend-momo-city/src/main/java/com/wanted/momocity/report/presentation/api/ReportExceptionHandler.java
@@ -3,12 +3,16 @@ package com.wanted.momocity.report.presentation.api;
 import com.wanted.momocity.global.presentation.api.common.ApiErrorResponse;
 import com.wanted.momocity.global.presentation.api.common.ApiResponseCode;
 import com.wanted.momocity.report.domain.exception.ReportNotFoundException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 // report 패키지에서 발생하는 전용 예외를 API 응답으로 변환
+// @Order(HIGHEST_PRECEDENCE) : 전역 ApiExceptionHandler 의 catch-all(Exception.class) 보다 먼저 처리되도록 우선순위 설정
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @RestControllerAdvice(basePackages = "com.wanted.momocity.report")
 public class ReportExceptionHandler {
 
