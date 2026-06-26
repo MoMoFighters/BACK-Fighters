bb600fa8e4e4cd45bc661793f30134c90846a42b
mosungjin <msj69076907@gmail.com>
2026-06-26 16:00:44 +0900

    [PR] CodeRabbit에 맞게 수정

--- legend-momo-city/src/main/java/com/wanted/momocity/enrollment/infrastructure/persistence/BuildingJpaEntity.java
+++ legend-momo-city/src/main/java/com/wanted/momocity/enrollment/infrastructure/persistence/BuildingJpaEntity.java
@@ -6,7 +6,10 @@ import com.wanted.momocity.global.domain.model.Category;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "building")
+// 유저가 같은 카테고리의 건물 중복 설정 방지를 위한 테이블 설정
+@Table(name = "building", uniqueConstraints = { // 매핑할 DB 테이블 이름과 유니크 제약 조건 설정
+        @UniqueConstraint(columnNames = {"user_Id", "category"}) // 사용자 별 같은 카테고리 건물 중복 생성 방지
+})
 public class BuildingJpaEntity extends BaseTimeEntity {
 
     @Id
