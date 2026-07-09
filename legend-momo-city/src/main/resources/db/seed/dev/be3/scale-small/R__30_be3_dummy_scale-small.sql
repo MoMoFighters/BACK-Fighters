f119622a32967d4551938d706ff8d89433947115
imnotquaso <sulley2626@gmai.com>
2026-07-09 21:51:46 +0900

    마이그레이션 더미데이터 및 스키마 수정

--- legend-momo-city/src/main/resources/db/seed/dev/be3/scale-small/R__30_be3_dummy_scale-small.sql
+++ legend-momo-city/src/main/resources/db/seed/dev/be3/scale-small/R__30_be3_dummy_scale-small.sql
@@ -1,14 +1,6 @@
 USE `momo`;
 SET FOREIGN_KEY_CHECKS = 0;
 
-TRUNCATE TABLE `notification`;
-TRUNCATE TABLE `guestbook`;
-TRUNCATE TABLE `message_read`;
-TRUNCATE TABLE `message_announce`;
-TRUNCATE TABLE `message`;
-TRUNCATE TABLE `chat_room_member`;
-TRUNCATE TABLE `chat_room`;
-
 -- =====================================================================
 --  1. chat_room
 -- =====================================================================
@@ -30,11 +22,11 @@ INSERT INTO `chat_room_member` (`id`, `joined_at`, `room_id`, `user_id`) VALUES
 --  3. message
 -- =====================================================================
 INSERT INTO `message` (`id`, `content`, `created_at`, `updated_at`, `room_id`, `sender_id`) VALUES
-  (1, '안녕하세요!', NOW() - INTERVAL 3 DAY, NOW() - INTERVAL 3 DAY, 1, 12),
-  (2, '반가워요 :)', NOW() - INTERVAL 3 DAY + INTERVAL 1 HOUR, NOW() - INTERVAL 3 DAY + INTERVAL 1 HOUR, 1, 13),
-  (3, '오늘 운동하셨나요?', NOW() - INTERVAL 2 DAY, NOW() - INTERVAL 2 DAY, 2, 12),
-  (4, '네 오늘 완료했어요!', NOW() - INTERVAL 2 DAY + INTERVAL 30 MINUTE, NOW() - INTERVAL 2 DAY + INTERVAL 30 MINUTE, 2, 14),
-  (5, '같이 스터디할래요?', NOW() - INTERVAL 1 DAY, NOW() - INTERVAL 1 DAY, 3, 13),
+  (1,  '안녕하세요!',  NOW() - INTERVAL 3 DAY,  NOW() - INTERVAL 3 DAY,  1,  12),
+  (2,  '반가워요 :)',  NOW() - INTERVAL 3 DAY + INTERVAL 1 HOUR,  NOW() - INTERVAL 3 DAY + INTERVAL 1 HOUR,  1,  13),
+  (3,  '오늘 운동하셨나요?',  NOW() - INTERVAL 2 DAY,  NOW() - INTERVAL 2 DAY,  2,  12),
+  (4,  '네 오늘 완료했어요!',  NOW() - INTERVAL 2 DAY + INTERVAL 30 MINUTE,  NOW() - INTERVAL 2 DAY + INTERVAL 30 MINUTE,  2,  14),
+  (5,  '같이 스터디할래요?',  NOW() - INTERVAL 1 DAY,  NOW() - INTERVAL 1 DAY,  3,  13),
   (6, '좋아요!', NOW() - INTERVAL 12 HOUR, NOW() - INTERVAL 12 HOUR, 3, 15);
 -- =====================================================================
 --  4. message_read
@@ -49,19 +41,19 @@ INSERT INTO `message_read` (`id`, `is_deleted`, `is_msg_read`, `is_noti_read`, `
 -- =====================================================================
 --  5. guestbook
 -- =====================================================================
-INSERT INTO `guestbook` (`id`, `content`, `created_at`, `owner_id`, `writer_id`) VALUES
-  (1, '잘 부탁드려요!', NOW() - INTERVAL 30 DAY, 12, 13),
-  (2, '응원합니다!', NOW() - INTERVAL 25 DAY, 13, 12),
-  (3, '같이 열심히 해봐요 :)', NOW() - INTERVAL 20 DAY, 14, 12),
-  (4, '오늘도 화이팅!', NOW() - INTERVAL 10 DAY, 12, 15),
-  (5, '잘 지내시죠?', NOW() - INTERVAL 5 DAY, 15, 14);
+INSERT INTO `guestbook` (`id`, `is_read`, `content`, `created_at`, `owner_id`, `writer_id`) VALUES
+  (1, FALSE, '잘 부탁드려요!', NOW() - INTERVAL 30 DAY, 12, 13),
+  (2, TRUE, '응원합니다!', NOW() - INTERVAL 25 DAY, 13, 12),
+  (3, FALSE, '같이 열심히 해봐요 :)', NOW() - INTERVAL 20 DAY, 14, 12),
+  (4, TRUE, '오늘도 화이팅!', NOW() - INTERVAL 10 DAY, 12, 15),
+  (5, FALSE, '잘 지내시죠?', NOW() - INTERVAL 5 DAY, 15, 14);
 -- =====================================================================
 --  6. notification
 -- =====================================================================
 INSERT INTO `notification` (`id`, `created_at`, `is_read`, `message`, `ref_id`, `type`, `user_id`) VALUES
-  (1, NOW() - INTERVAL 2 DAY, 0, '김민수님이 댓글을 달았습니다', 1, 'COMMENT', 13),
-  (2, NOW() - INTERVAL 1 DAY, 0, '이지영님이 좋아요를 눌렀습니다', 1, 'LIKE', 12),
-  (3, NOW() - INTERVAL 12 HOUR, 1, '새 강의가 등록되었습니다', 1, 'LECTURE', 14),
-  (4, NOW() - INTERVAL 6 HOUR, 0, '친구 요청이 왔습니다', 15, 'FRIEND', 12);
+  (1, NOW() - INTERVAL 2 DAY, 0, '김민수님이 댓글을 달았습니다', 1, 'POST', 13),
+  (2, NOW() - INTERVAL 1 DAY, 0, '이지영님이 좋아요를 눌렀습니다', 1, 'POST', 12),
+  (3, NOW() - INTERVAL 12 HOUR, 1, '새 강의가 등록되었습니다', 1, 'NOTICE', 14),
+  (4, NOW() - INTERVAL 6 HOUR, 0, '친구 요청이 왔습니다', 15, 'FRIEND_REQUEST', 12);
 
 SET FOREIGN_KEY_CHECKS = 1;
--- legend-momo-city/src/main/resources/db/seed/dev/be4/scale-small/R__40_be4_dummy_scale-small.sql
+++ legend-momo-city/src/main/resources/db/seed/dev/be4/scale-small/R__40_be4_dummy_scale-small.sql
@@ -1,36 +1,21 @@
 USE `momo`;
 SET FOREIGN_KEY_CHECKS = 0;
 
-TRUNCATE TABLE `order_history`;
-TRUNCATE TABLE `store`;
-TRUNCATE TABLE `user_oauth`;
-
 -- =====================================================================
 --  1. user_oauth
 -- =====================================================================
 INSERT INTO `user_oauth` (`id`, `created_at`, `provider`, `provider_id`, `user_id`) VALUES
-  (1, NOW() - INTERVAL 24 DAY, 1, 'kakao_001', 29),
+  (1, NOW() - INTERVAL 24 DAY, 'KAKAO', 'kakao_001', 29),
   (2, NOW() - INTERVAL 18 DAY, 2, 'google_001', 30);
 -- =====================================================================
 --  2. store
 -- =====================================================================
-INSERT INTO `store` (`id`, `price`, `type`, `url`, `name`, `created_at`) VALUES
-  (1, 30, 'PROFILE', 'https://momocity-bucket.s3.ap-northeast-2.amazonaws.com/store/item01.png', 'cat1', NOW() - INTERVAL 57 DAY),
-  (2, 30, 'PROFILE', 'https://momocity-bucket.s3.ap-northeast-2.amazonaws.com/store/item02.png', 'cat2', NOW() - INTERVAL 54 DAY),
-  (3, 50, 'PROFILE', 'https://momocity-bucket.s3.ap-northeast-2.amazonaws.com/store/item03.png', 'cat3', NOW() - INTERVAL 51 DAY),
-  (4, 50, 'PROFILE', 'https://momocity-bucket.s3.ap-northeast-2.amazonaws.com/store/item04.png', 'cat4', NOW() - INTERVAL 48 DAY),
-  (5, 50, 'PROFILE', 'https://momocity-bucket.s3.ap-northeast-2.amazonaws.com/store/item05.png', 'dog1', NOW() - INTERVAL 45 DAY),
-  (6, 80, 'PROFILE', 'https://momocity-bucket.s3.ap-northeast-2.amazonaws.com/store/item06.png', 'dog2', NOW() - INTERVAL 42 DAY),
-  (7, 80, 'PROFILE', 'https://momocity-bucket.s3.ap-northeast-2.amazonaws.com/store/item07.png', 'dog3', NOW() - INTERVAL 39 DAY),
-  (8, 100, 'PROFILE', 'https://momocity-bucket.s3.ap-northeast-2.amazonaws.com/store/item08.png', 'dog4', NOW() - INTERVAL 36 DAY),
-  (9, 100, 'PROFILE', 'https://momocity-bucket.s3.ap-northeast-2.amazonaws.com/store/item09.png', 'penguin1', NOW() - INTERVAL 33 DAY),
-  (10, 100, 'PROFILE', 'https://momocity-bucket.s3.ap-northeast-2.amazonaws.com/store/item10.png', 'penguin2', NOW() - INTERVAL 30 DAY),
-  (11, 120, 'PROFILE', 'https://momocity-bucket.s3.ap-northeast-2.amazonaws.com/store/item11.png', 'penguin3', NOW() - INTERVAL 27 DAY),
-  (12, 120, 'PROFILE', 'https://momocity-bucket.s3.ap-northeast-2.amazonaws.com/store/item12.png', 'penguin4', NOW() - INTERVAL 24 DAY),
-  (13, 150, 'PROFILE', 'https://momocity-bucket.s3.ap-northeast-2.amazonaws.com/store/item13.png', 'rabbit1', NOW() - INTERVAL 21 DAY),
-  (14, 150, 'PROFILE', 'https://momocity-bucket.s3.ap-northeast-2.amazonaws.com/store/item14.png', 'rabbit2', NOW() - INTERVAL 18 DAY),
-  (15, 200, 'PROFILE', 'https://momocity-bucket.s3.ap-northeast-2.amazonaws.com/store/item15.png', 'rabbit3', NOW() - INTERVAL 15 DAY),
-  (16, 200, 'PROFILE', 'https://momocity-bucket.s3.ap-northeast-2.amazonaws.com/store/item16.png', 'rabbit4', NOW() - INTERVAL 12 DAY);
+INSERT INTO `store` (`id`, `created_at`, `name`, `price`, `type`, `url`) VALUES
+  (1, NOW() - INTERVAL 120 DAY, '프로필 이미지 1', 100, 'PROFILE', 'https://momocity-bucket.s3.ap-northeast-2.amazonaws.com/store/item01.png'),
+  (2, NOW() - INTERVAL 120 DAY, '프로필 이미지 2', 150, 'PROFILE', 'https://momocity-bucket.s3.ap-northeast-2.amazonaws.com/store/item02.png'),
+  (3, NOW() - INTERVAL 120 DAY, '프로필 이미지 3', 200, 'PROFILE', 'https://momocity-bucket.s3.ap-northeast-2.amazonaws.com/store/item03.png'),
+  (4, NOW() - INTERVAL 120 DAY, '프로필 이미지 4', 250, 'PROFILE', 'https://momocity-bucket.s3.ap-northeast-2.amazonaws.com/store/item04.png'),
+  (5, NOW() - INTERVAL 120 DAY, '프로필 이미지 5', 300, 'PROFILE', 'https://momocity-bucket.s3.ap-northeast-2.amazonaws.com/store/item05.png');
 -- =====================================================================
 --  3. order_history
 -- =====================================================================
