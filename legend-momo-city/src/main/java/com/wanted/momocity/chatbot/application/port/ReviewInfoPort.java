701b5ec815d92950ec2a9c73d3ca5a79fc8595a7
mnppi223 <mnppi223@gmail.com>
2026-07-12 11:13:19 +0900

    [FEAT] Port 임시 개발 저장

--- legend-momo-city/src/main/java/com/wanted/momocity/chatbot/application/port/ReviewInfoPort.java
+++ legend-momo-city/src/main/java/com/wanted/momocity/chatbot/application/port/ReviewInfoPort.java
@@ -1,4 +1,14 @@
 package com.wanted.momocity.chatbot.application.port;
 
-public class ReviewInfoPort {
+/* comment.
+    Chatbot BC 가 review 도메인의 수강평 원문을 필요로 할 때, review 내부 구조를 직접
+    알지 않고 이 포트를 통해서만 접근 가능한 경계
+ */
+
+import java.util.List;
+
+public interface ReviewInfoPort {
+
+    List<String> getReviewContents(Long lectureId);
+
 }
