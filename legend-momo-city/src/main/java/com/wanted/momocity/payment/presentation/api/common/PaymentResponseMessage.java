c88e168cffd159fe4289b6203f4d22814f49530c
sooyoung0927 <sooyoung040927@gmail.com>
2026-07-14 14:33:28 +0900

    코드레빗 반영

--- legend-momo-city/src/main/java/com/wanted/momocity/payment/presentation/api/common/PaymentResponseMessage.java
+++ legend-momo-city/src/main/java/com/wanted/momocity/payment/presentation/api/common/PaymentResponseMessage.java
@@ -6,7 +6,7 @@ public final class PaymentResponseMessage {
 
     public static final String PAYMENT_READY = "결제 준비 완료";
     public static final String PAYMENT_VERIFIED = "결제 검증 완료";
-    public static final String SUBSCRIBE_CANCEL = "BASIC플랜으로 변경되었습니다.";
+    public static final String SUBSCRIBE_CANCEL = "환불처리 되었습니다. 멤버십이 BASIC으로 전환됩니다.";
     public static final String FETCH_SUCCESS = "조회 성공";
 
 }
